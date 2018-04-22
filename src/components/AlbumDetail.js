bfbd820663e34ffe7f4f1097ca97f6903686ff45
faalbane <faalbane@gmail.com>
2018-04-21 21:05:31 -0400

    first commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,73 @@
+// Import a library to help make a component
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native'; //
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+// Make a component
+const AlbumDetail = ({ album }) => {
+
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle, imageStyle, headerTextStyle, headerContentStyle, thumbnailContainerStyle } = styles;
+
+    return (
+        <Card>
+
+            <CardSection>
+
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+
+            </CardSection>
+
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Purchase
+                </Button>
+            </CardSection>
+
+        </Card>
+    );
+}
+
+const styles = {
+
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        //paddingLeft: 5
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }
+}
+
+// Make the component available to other parts of the app
+export default AlbumDetail;
+
